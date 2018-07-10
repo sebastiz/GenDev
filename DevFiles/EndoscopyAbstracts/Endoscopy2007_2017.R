88e629e0cdb49f3baddac6cab8d8d3b86998a072
sebastiz <sebastiz@hotmail.com>
2018-07-10 13:35:17 +0000

    Organised and added automated demographics

--- DevFiles/EndoscopyAbstracts/Endoscopy2007_2017.R
+++ DevFiles/EndoscopyAbstracts/Endoscopy2007_2017.R
@@ -1,8 +1,22 @@
 library(readxl)
 library(EndoMineR)
+library(stringr)
+library(dplyr)
+library(lubridate)
+library(psych)
+library(zoo)
+
+
+
+
+#######################################????????????????Data acquisiton######################################### 
+#This is where the data is imported
 Mydata<-data.frame(read_excel("S:/Gastroenterology/Seb/R/Data/AdHoc/GSTT_GastroscopyProcedure2007_2017.xlsx"))
 names(Mydata)<-c("PatientID","NHSno","PatientName","birthdaynum","birthmonthnum","birthyearnum","Endo_ResultName","Endo_ResultPerformed","Endo_ResultEntered","Endo_ResultText","Histo_ResultName","Histo_ResultPerformed","Histo_ResultEntered","Histo_ResultText")
 
+#######################################????????????????Data merging######################################### 
+#This is where external datasets are merged with the current dataset. This can be done after cleaning as well especially if you need to clean dates etc.
+#######################################????????????????Data cleaning######################################### 
 
 
 RenameCols<-function(x){
@@ -75,6 +89,35 @@ for(i in 1:(length(EndoTree)-1)) {
 }
 
 
+Mydata<-EndoscEndoscopist(Mydata,'Endoscopist')
+Mydata$Endoscopist<-gsub("Second","",Mydata$Endoscopist)
+Mydata<-EndoscMeds(Mydata,'Medications')
+Mydata<-EndoscInstrument(Mydata,'Instrument')
+Mydata<-EndoscIndications(Mydata,'INDICATIONSFOREXAMINATION')
+Mydata<-EndoscProcPerformed(Mydata,'PROCEDUREPERFORMED')
+
+
+Mydata<-HistolrHistol(Mydata,'HISTOLOGY')
+Mydata<-HistolrDx(Mydata,"DIAGNOSIS")
+Mydata<-HistolExtrapolDx(Mydata,"DIAGNOSIS")
+
+
+
+######################################################????????????????Data forking (filtering and subsetting)######################################### 
+
+
+
+#To do: Decide on retrospective cohort question types that often get asked as generic questions then code that up
+
+
+############                Adenomas         ############
+
+dataframe <- Self[grepl("Colonoscopy", Self$PROCEDUREPERFORMED),]
+MyAdenomaDataFrame <- dataframe[grep(".*[Aa]denom.*", dataframe$Dx),]
+
+
+
+############                Eosinophil         ############
 #Get rid of 'no evidence of'
 MydataDx<-Mydata[grep("osinop",Mydata$DIAGNOSIS),]
 MydataDx$DIAGNOSIS<-gsub("\n","-",MydataDx$DIAGNOSIS,fixed=TRUE)
@@ -82,8 +125,30 @@ MydataDx<-MydataDx[!grepl(".*[Nn]o\\s*evidence\\s*of\\s*[Ee]osinop.*",MydataDx$D
 MydataDx<-MydataDx[!grepl(".*histological evidence\\s+of\\s+.*[Ee]osinop.*",MydataDx$Histo_ResultText),]
 EoEDx<-MydataDx
 
-library(zoo)
-library(stringr)
+############                Barretts         ############
+MyBarrettsData<-Mydata[grepl("Barr",Mydata$FINDINGS),]
+
+
+
+
+#######################################????????????????Data accordionisation ############################################################
+
+
+############                Adenomas         ############
+#Remove the previous polyp rows
+MyAdenomaDataFrame<-MyAdenomaDataFrame[!grepl("Surveillance- Previous Polyps",MyAdenomaDataFrame$INDICATIONSFOREXAMINATION),]
+
+# Get the number of adenomas diagnosed by year
+MyAdenomaDataFrame%>%arrange(as.Date(Endo_ResultPerformed)) %>% group_by(
+  year = year(as.Date(Endo_ResultPerformed))
+) %>% summarise(Number = n())
+
+
+
+
+############                Eosinophil         ############
+
+
 EoEDx$HPF<-as.numeric(sapply(str_extract_all(EoEDx$Histo_ResultText, "[0-9]+(?=(\\s+[Ii]ntraepithelial)?(\\s+[Ee]osinophils)?\\s+(per|in one|in a|\\/)?.+([Hh][Pp][Ff]|[Hh]igh.+power.+field|[Hh]ighpower\\s+field))"), 
                              function(x) x[which.max(as.numeric(x))][1]))
 
@@ -92,41 +157,55 @@ EoEDx$HPF<-as.numeric(sapply(str_extract_all(EoEDx$Histo_ResultText, "[0-9]+(?=(
 PatientsWithEoE<-subset(EoEDx,EoEDx$HPF>=15)
 
 
-
-#Export as excel speadsheet
-
-writeWorksheetToFile("~\\AnothEoEForIona.xlsx",data=PatientsWithEoE,sheet="blabla",startRow=3,startCol=4)
-
-
-
-Mydata<-EndoscChopperEndoscopist(Mydata,'Endoscopist')
-Mydata$Endoscopist<-gsub("Second","",Mydata$Endoscopist)
-Mydata<-EndoscChopperMeds(Mydata,'Medications')
-Mydata<-EndoscChopperInstrument(Mydata,'Instrument')
-Mydata<-EndoscChopperIndications(Mydata,'INDICATIONSFOREXAMINATION')
-Mydata<-EndoscChopperProcPerformed(Mydata,'PROCEDUREPERFORMED')
-
-
-Mydata<-HistolChopperHistol(Mydata,'HISTOLOGY')
-Mydata<-HistolChopperDx(Mydata,"DIAGNOSIS")
-Mydata<-HistolChopperExtrapolDx(Mydata,"DIAGNOSIS")
-
-
-
-MyBarrettsData<-Mydata[grepl("Barr",Mydata$FINDINGS),]
+############                Barretts         ############
 em<-SurveillanceCapacity(MyBarrettsData,"Endo_ResultPerformed")
 how<-HowManyTests(MyBarrettsData,'INDICATIONSFOREXAMINATION','Endo_ResultPerformed','Surv')
 b1<-BarrettsDataAccord_Prague(MyBarrettsData,'FINDINGS')
 b2<-BarrettsDataAccord_PathStage(b1,'HISTOLOGY')
 b3<-BarrettsDataAccord_Event(b2,'HISTOLOGY','PROCEDUREPERFORMED','Endo_ResultText','FINDINGS')
 b4<-BarrettsDataAccord_FUGroup(b3,'FINDINGS')
-
 Rule<-BarrettsPatientTracking_UniqueHospNum(b4,'Rule1','HospitalNumber')
-
-
 BarrettsQuality_AnalysisDocumentation(b4,"FINDINGS")
 BarrettsSurveillance_PathDetection(b4,'Myplot')
-
 Therapeutics<-b4[!grepl("nothing",b4$EVENT),]
 
+
+
+#######################################????????????????Subset merging with other datasets ################ 
+
+
+
+#######################################????????????????Cohort Question Type 1 ################ 
+#######################################????????????????Cohort Question Type 2 ################ 
+#######################################????????????????Cohort Question Type 3 ################ 
+#######################################????????????????Cohort Question Type 4 ################ 
+#######################################????????????????Cohort Question Type 5 ################ 
+#######################################????????????????Cohort Question Type 6 ################ 
+
+
+
+#######################################????????????????Demographics for subset ################ 
+#M:F ratio (extract from the title in the Patient name column
+Mydata$Gender<-ifelse(grepl("MRS|MS|MISS", Mydata$PatientName), "Female","Male")
+
+genderStats<-Mydata%>%group_by(Gender)%>%summarise(n=n())
+
+MFRatio<-genderStats[2,2]/genderStats[1,2]
+#Age and range of age (age at diagnosis)
+Mydata$DiagnosisYear<-lubridate::year(Mydata$Endo_ResultEntered)
+Mydata$AgeAtDiagnosis<-Mydata$DiagnosisYear-Mydata$birthyearnum
+summ<-describe(Mydata$AgeAtDiagnosis) 
+
+summ$median
+summ$mean
+summ$range
+summ$max
+summ$min
+summ$sd
+summ$n
 BarrettsTherapeuticsRFA_ByCatheter(Therapeutics,"FINDINGS","ENDOSCOPICDIAGNOSIS")
+
+
+
+
+
---
+++ DevFiles/EndoscopyAbstracts/Endoscopy2007_2017v2.R
@@ -0,0 +1,59 @@
+library(EndoMineR)
+library(readxl)
+
+
+
+######################################### Data acquisiton######################################### 
+#This is where the data is imported
+Self<-read_excel("/home/rstudio/EndoMineR/data/GSTT_TCR2232_AllEndoscopyProcedure_since_2007.xlsx")
+
+######################################### Data merging######################################### 
+#This is where external datasets are merged with the current dataset. This can be done after cleaning as well especially if you need to clean dates etc.
+######################################### Data cleaning######################################### 
+
+
+names(Self)<-c("PatientID",  "NHSno", "PatientName", "birthdaynum","birthmonthnum" ,"birthyearnum" ,"Endo_ResultName", "Endo_ResultPerformed", "Endo_ResultEntered", "Endo_ResultText", "Histo_ResultName", "Histo_ResultPerformed", "Histo_ResultEntered", "Histo_ResultText")
+
+#Remove the first row if necessary
+Self<-subset(Self,!(is.na(Self["Endo_ResultName"])))
+
+
+
+#Use Extractor2 as delimiters are organised in a non-consistent way:
+Self$Endo_ResultText<-gsub('2nd Endoscopist:','Second endoscopist:',
+                           Self$Endo_ResultText)
+EndoscTree<-list("Patient Name:","Date of Birth:","Hospital Number:","Date of Procedure:","Endoscopist:","Second endoscopist:",
+               "Referring Physician:","General Practicioner:","Nurses:",
+               "Medications:","Instrument:","Extent of Exam:","Visualization:","Tolerance:","Complications:","Co-morbidity:",
+               "INDICATIONS FOR EXAMINATION","PROCEDURE PERFORMED",
+               "FINDINGS","ENDOSCOPIC DIAGNOSIS","RECOMMENDATIONS","COMMENTS","BIOPSIES","OPCS4 Code:","Signature:","")
+for(i in 1:(length(EndoscTree)-1)) {
+  Self<-Extractor2(Self,'Endo_ResultText',as.character(EndoscTree[i]),
+                     as.character(EndoscTree[i+1]),as.character(EndoscTree[i]))
+}
+
+
+HistolTree<-c("NATURE OF SPECIMEN:","CLINICAL DETAILS","MACROSCOPICAL DESCRIPTION","HISTOLOGY",
+              "DIAGNOSIS","")
+for(i in 1:(length(HistolTree)-1)) {
+  Self<-Extractor2(Self,"Histo_ResultText",as.character(HistolTree[i]),
+                    as.character(HistolTree[i+1]),as.character(HistolTree[i]))
+}
+
+
+
+
+
+######################################### Data forking (filtering and subsetting)######################################### 
+
+Self<-EndoscEndoscopist(Self,'Endoscopist')
+Self$Endoscopist<-gsub("Second","",Self$Endoscopist)
+Self<-EndoscMeds(Self,'Medications')
+Self<-EndoscInstrument(Self,'Instrument')
+Self<-EndoscIndications(Self,'INDICATIONSFOREXAMINATION')
+Self<-EndoscProcPerformed(Self,'PROCEDUREPERFORMED')
+
+
+Self<-HistolHistol(Self,'HISTOLOGY')
+Self<-HistolDx(Self,"DIAGNOSIS")
+Self<-HistolExtrapolDx(Self,"DIAGNOSIS")
