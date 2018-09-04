e65b9154cfecb567e8dc1e8d4e255fb1e4b04363
sebastiz <sebastiz@hotmail.com>
2018-09-04 11:34:45 +0000

    Cleaning up the GenDev with removal of files. Also making the Symptom extractor more generic

--- DevFiles/Generics/ClinicalGenerics.R
+++ DevFiles/Generics/ClinicalGenerics.R
@@ -4,14 +4,14 @@
 #Symptom extraction
 
 SymptomsExtractions<-function(x,y){
-data$Dysphagia<-ifelse(grepl(".*[Dd]ysph.*",data$IndicANDHx,perl=TRUE)|grepl(".*[Oo]dyn.*",data$IndicANDHx,perl=TRUE)|grepl(".*[Ss]tuck.*",data$IndicANDHx,perl=TRUE)|grepl(".*[Ss]tick.*",data$IndicANDHx,perl=TRUE),"Yes","No")
-data$Heartburn<-ifelse(grepl(".*[Hh]eart.*",data$IndicANDHx,perl=TRUE)|grepl(".*[Rr]eflu.*",data$IndicANDHx,perl=TRUE)|grepl(".*[Rr]etro.*",data$IndicANDHx,perl=TRUE)|grepl(".*[Bb]urn.*",data$IndicANDHx,perl=TRUE),"Yes","No")
-data$Throat<-ifelse(grepl(".*[Tt]hroat.*",data$IndicANDHx,perl=TRUE)|grepl(".*[Nn]eck.*",data$IndicANDHx,perl=TRUE),"Yes","No")
-data$Cough<-ifelse(grepl(".*[Cc]ough.*",data$IndicANDHx,perl=TRUE)|grepl(".*[Cc]hok.*",data$IndicANDHx,perl=TRUE),"Yes","No")
-data$ChestPain<-ifelse(grepl(".*[Ch]est.*",data$IndicANDHx,perl=TRUE),"Yes","No")
-data$AbdoPain<-ifelse(grepl(".*[Ss]tomach.*",data$IndicANDHx,perl=TRUE)|grepl(".*[Ee]piga.*",data$IndicANDHx,perl=TRUE)|grepl(".*[Aa]bdom.*",data$IndicANDHx,perl=TRUE),"Yes","No")
-data$Hoarseness<-ifelse(grepl(".*[Hh]oarse.*",data$IndicANDHx,perl=TRUE),"Yes","No")
-data$Regurgitation<-ifelse(grepl(".*[Rr]egur.*",data$IndicANDHx,perl=TRUE)|grepl(".*[Tt]aste.*",data$IndicANDHx,perl=TRUE),"Yes","No")
-data$Vomiting<-ifelse(grepl(".*[Vv]omit.*",data$IndicANDHx,perl=TRUE),"Yes","No")
-data$Belch<-ifelse(grepl(".*[Bb]elch.*",data$IndicANDHx,perl=TRUE)|grepl(".*[Bb]urp.*",data$IndicANDHx,perl=TRUE),"Yes","No")
+x$Dysphagia<-ifelse(grepl(".*[Dd]ysph.*",x$y,perl=TRUE)|grepl(".*[Oo]dyn.*",x$y,perl=TRUE)|grepl(".*[Ss]tuck.*",x$y,perl=TRUE)|grepl(".*[Ss]tick.*",x$y,perl=TRUE),"Yes","No")
+x$Heartburn<-ifelse(grepl(".*[Hh]eart.*",x$y,perl=TRUE)|grepl(".*[Rr]eflu.*",x$y,perl=TRUE)|grepl(".*[Rr]etro.*",x$y,perl=TRUE)|grepl(".*[Bb]urn.*",x$y,perl=TRUE),"Yes","No")
+x$Throat<-ifelse(grepl(".*[Tt]hroat.*",x$y,perl=TRUE)|grepl(".*[Nn]eck.*",x$y,perl=TRUE),"Yes","No")
+x$Cough<-ifelse(grepl(".*[Cc]ough.*",x$y,perl=TRUE)|grepl(".*[Cc]hok.*",x$y,perl=TRUE),"Yes","No")
+x$ChestPain<-ifelse(grepl(".*[Ch]est.*",x$y,perl=TRUE),"Yes","No")
+x$AbdoPain<-ifelse(grepl(".*[Ss]tomach.*",x$y,perl=TRUE)|grepl(".*[Ee]piga.*",x$y,perl=TRUE)|grepl(".*[Aa]bdom.*",x$y,perl=TRUE),"Yes","No")
+x$Hoarseness<-ifelse(grepl(".*[Hh]oarse.*",x$y,perl=TRUE),"Yes","No")
+x$Regurgitation<-ifelse(grepl(".*[Rr]egur.*",x$y,perl=TRUE)|grepl(".*[Tt]aste.*",x$y,perl=TRUE),"Yes","No")
+x$Vomiting<-ifelse(grepl(".*[Vv]omit.*",x$y,perl=TRUE),"Yes","No")
+x$Belch<-ifelse(grepl(".*[Bb]elch.*",x$y,perl=TRUE)|grepl(".*[Bb]urp.*",x$y,perl=TRUE),"Yes","No")
 }
--- DevFiles/Generics/tbnbv2.R
+++
@@ -1,5 +0,0 @@
-source("S:\\Gastroenterology\\Seb\\R\\Scripts\\Generics\\CleanUp.R")
-d="/Users/sebastianzeki/Dropbox/Work/Medical/Clinical/Gastro/EndoHistolToolScript/EndoscopyQueriesOGD/Colonscopy_HistoReport.xlsx"
-sh<-as.data.frame(mysummary(d))
-
-
